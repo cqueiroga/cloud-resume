55ee99df14646b9c7184be99902ab65dc3ddde8a
cqueiroga <chris.queiroga@gmail.com>
2024-04-30 19:30:07 -0400

    Updated js for visitor counter. Updated lambda, api gateway and dynamodb in aws.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 const counter = document.querySelector("#count");
 
 async function updateCounter() {
-    let response = await fetch("https://gap4zp0djk.execute-api.us-east-1.amazonaws.com/dev");
-    let data = await response.json();
-    counter.innerHTML = data
+	let response = await fetch(
+		"https://ggwekz8ss0.execute-api.us-east-1.amazonaws.com/prod"
+	);
+	let data = await response.json();
+	counter.innerHTML = data;
 }
 
 updateCounter();
